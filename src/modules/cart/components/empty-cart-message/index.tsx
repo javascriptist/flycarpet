3a074f71ba75993e66a0f81ad6eaa1273ab58341
abdurakhmanov.jpg <test@Mukhammadkodirs-MacBook-Air.local>
2025-12-09 19:25:47 +0000

    Fix product button

--- src/modules/cart/components/empty-cart-message/index.tsx
+++ src/modules/cart/components/empty-cart-message/index.tsx
@@ -4,8 +4,6 @@ import InteractiveLink from "@modules/common/components/interactive-link"
 import { t } from '@lib/util/translations'
 
 const EmptyCartMessage = ({ countryCode }: { countryCode?: string }) => {
-  const isLang = countryCode === "uz"
-  
   return (
     <div className="py-48 px-2 flex flex-col justify-center items-start" data-testid="empty-cart-message">
       <Heading
@@ -15,7 +13,7 @@ const EmptyCartMessage = ({ countryCode }: { countryCode?: string }) => {
         {t({ uz: 'Savatcha', ru: 'Корзина', en: 'Cart' }, countryCode)}
       </Heading>
       <p className="text-base-regular mt-4 mb-6 max-w-[32rem]">
-        {t({ uz: 'Savatchada hech narsa yo\u02bcq. Keling, buni o\u02bzgartiramiz, mahsulotlarni ko\u02bcrishni boshlash uchun quyidagi havoladan foydalaning.', ru: 'В вашей корзине ничего нет. Давайте это изменим, используйте ссылку ниже, чтобы начать просмотр наших товаров.', en: 'Your cart is empty. Let\'s change that, use the link below to start browsing our products.' }, countryCode)}
+        {t({ uz: 'Savatchada hech narsa yo\u02bcq. Keling, buni o\u02bczgartiramiz, mahsulotlarni ko\u02bcrishni boshlash uchun quyidagi havoladan foydalaning.', ru: 'В вашей корзине ничего нет. Давайте это изменим, используйте ссылку ниже, чтобы начать просмотр наших товаров.', en: 'Your cart is empty. Let\'s change that, use the link below to start browsing our products.' }, countryCode)}
       </p>
       <div>
         <InteractiveLink href="/store">
